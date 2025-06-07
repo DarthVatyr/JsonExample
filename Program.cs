06f219e78aa775ac4f5ddee039bce2ef162b25e2
DarthVatyr <jmriddle.it@gmail.com>
2025-06-07 19:48:40 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string json = @"{ ""Name"": ""Alice"", ""Age"": 30 }";
+        Person person = JsonConvert.DeserializeObject<Person>(json);
+
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+
+        // Create a new Person object
+        Person newPerson = new Person { Name = "Bob", Age = 25 };
+
+        // Serialize the Person object to a JSON string
+        string newJson = JsonConvert.SerializeObject(newPerson);
+
+        Console.WriteLine($"Serialized JSON: {newJson}");
+    }
+}
